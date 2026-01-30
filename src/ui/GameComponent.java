574f509dc8636142560f132a57386a98eb205075
rhit-letojj <letojj@rose-hulman.edu>
2026-01-30 14:16:48 -0500

    Added Map UI and sprites

--- src/ui/GameComponent.java
+++ src/ui/GameComponent.java
@@ -2,20 +2,46 @@ package ui;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
-import model.GameModel;
+import model.GameLogic;
 
 public class GameComponent extends JComponent {
 
 	
 	
-	private GameModel model;
+	private GameLogic model;
+	
+	int[][] tiles = {
+			{1,1,1,1,1,1,1,1,2,1},
+			{1,1,1,0,0,0,0,0,0,1},
+			{1,1,1,1,0,0,1,1,1,1},
+			{1,0,0,0,0,0,1,0,0,1},
+			{1,1,1,0,0,0,1,1,0,1},
+			{1,0,1,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,1},
+			{1,0,0,1,1,1,1,1,1,1},
+			{1,0,0,1,0,0,0,0,0,1},
+			{1,1,1,1,1,1,1,1,1,1}
+	};
+	private Image[] tileImages;
+	private final int TILE_SIZE = 80;
 
 
-	public GameComponent(GameModel model) {
+	public GameComponent(GameLogic model) throws IOException {
 	this.model = model;
+	this.tileImages = new Image[3];
+	try {
+        tileImages[0] = ImageIO.read(getClass().getResource("grassTile.png"));
+        tileImages[1] = ImageIO.read(getClass().getResource("rockTile.png"));
+        tileImages[2] = ImageIO.read(getClass().getResource("exitTile.png"));
+    } catch (IOException e) {
+        throw new RuntimeException("Failed to load tile images", e);
+    }
 	}
 
 
@@ -23,11 +49,16 @@ public class GameComponent extends JComponent {
 	protected void paintComponent(Graphics g) {
 	super.paintComponent(g);
 	Graphics2D g2 = (Graphics2D) g;
+	for (int y = 0; y < tiles.length; y++) {
+        for (int x = 0; x < tiles[y].length; x++) {
+            int tileId = tiles[y][x];
+            Image tileImage = tileImages[tileId];
 
-	// Minimal placeholder to test  it’s running
-	g2.drawString("Final Project Starter: UI is running ✅", 20, 30);
+            int dx = x * TILE_SIZE;
+            int dy = y * TILE_SIZE;
 
-
-	// TODO: draw based on model state
+            g.drawImage(tileImage, dx, dy, TILE_SIZE, TILE_SIZE, null);
+        }
+    }
 	}
 }
--- src/ui/GameWindow.java
+++ src/ui/GameWindow.java
@@ -1,24 +1,31 @@
 package ui;
 
+import java.io.IOException;
+
 import javax.swing.JFrame;
 
-import model.GameModel;
+import model.GameLogic;
 
 public class GameWindow {
 
 	public static void show() {
 		// Minimal model instance (empty for now, by design)
-		GameModel model = new GameModel();
+		GameLogic model = new GameLogic();
 
 
 		JFrame frame = new JFrame("CSSE220 Final Project");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-		frame.add(new GameComponent(model));
+		try {
+			frame.add(new GameComponent(model));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 
-		frame.setSize(600, 600);
+		frame.setSize(800, 800);
 		frame.setLocationRelativeTo(null); // center on screen (nice UX, still minimal)
 		frame.setVisible(true);
 		}
